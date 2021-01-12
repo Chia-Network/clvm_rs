110e5f82468f5dc8a0899610f8a8123bbcebb7e5
Arvid Norberg <arvid@libtorrent.org>
2021-01-12 14:08:10 +0100

    destruct PyNode iteratively rather than recursively, to avoid stack overflow

--- src/py/py_node.rs
+++ src/py/py_node.rs
@@ -207,3 +207,43 @@ impl From<&Arc<PyNode>> for PyNode {
         }
     }
 }
+
+fn move_out(n: &mut Arc<PySExp>) -> Arc<PySExp> {
+    // for some reason rust doesn't let us move members out of self when being
+    // destructed, so we need to replace it with a dummy allocation
+    return std::mem::replace(n, Arc::new(PySExp::Atom(Arc::new([0]))));
+}
+
+// to avoid stack overflow by destructing the tree of nodes recursively, first
+// move out all pointers with refcount 1 (get_mut() fails if there are multiple
+// references) and stick them in a (shallow) vector. That way, destruction will
+// happen sequentially, rather than recursively
+impl Drop for PyNode {
+    fn drop(&mut self) {
+        if Arc::strong_count(&mut self.node) > 1 {
+            return;
+        };
+        let mut vec = Vec::<Arc<PySExp>>::new();
+
+        let mut current = move_out(&mut self.node);
+        loop {
+            let (left, right) = match Arc::get_mut(&mut current) {
+                Some(PySExp::Pair(left, right)) => (Arc::get_mut(left), Arc::get_mut(right)),
+                _ => (None, None),
+            };
+
+            if let Some(v) = left {
+                vec.push(move_out(&mut v.node));
+            }
+            if let Some(v) = right {
+                vec.push(move_out(&mut v.node));
+            }
+            current = match vec.pop() {
+                Some(n) => n,
+                _ => {
+                    break;
+                }
+            };
+        }
+    }
+}
