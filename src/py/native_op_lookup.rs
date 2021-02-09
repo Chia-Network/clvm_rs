2d61ffb6f3fbcd4be86420c1913c119cd10335c6
Richard Kiss <him@richardkiss.com>
2021-02-08 16:28:16 -0800

    Use an inner class.

--- src/py/native_op_lookup.rs
+++ src/py/native_op_lookup.rs
@@ -1,3 +1,4 @@
+use crate::allocator::Allocator;
 use crate::node::Node;
 use crate::reduction::{EvalErr, Reduction};
 
@@ -11,9 +12,9 @@ use pyo3::types::{PyString, PyTuple};
 #[pyclass]
 #[derive(Clone)]
 pub struct NativeOpLookup {
-    py_callback: PyObject,
-    f_lookup: FLookup<ArcAllocator>,
+    nol: GenericNativeOpLookup,
 }
+
 
 #[pymethods]
 impl NativeOpLookup {
@@ -26,8 +27,10 @@ impl NativeOpLookup {
             f_lookup[idx] = native_lookup[idx];
         }
         NativeOpLookup {
-            py_callback: unknown_op_callback.into(),
-            f_lookup,
+            nol: GenericNativeOpLookup {
+                py_callback: unknown_op_callback.into(),
+                f_lookup,
+            },
         }
     }
 }
@@ -50,6 +53,25 @@ impl NativeOpLookup {
         op: &[u8],
         argument_list: &ArcSExp,
     ) -> Result<Reduction<ArcSExp>, EvalErr<ArcSExp>> {
+        self.nol.operator_handler(allocator, op, argument_list)
+    }
+}
+#[derive(Clone)]
+struct GenericNativeOpLookup {
+    py_callback: PyObject,
+    f_lookup: FLookup<ArcAllocator>,
+}
+
+impl GenericNativeOpLookup {
+    pub fn operator_handler(
+        &self,
+        allocator: &ArcAllocator,
+        op: &[u8],
+        argument_list: &<ArcAllocator as Allocator>::Ptr,
+    ) -> Result<
+        Reduction<<ArcAllocator as Allocator>::Ptr>,
+        EvalErr<<ArcAllocator as Allocator>::Ptr>,
+    > {
         if op.len() == 1 {
             if let Some(f) = self.f_lookup[op[0] as usize] {
                 let node_t: Node<ArcAllocator> = Node::new(allocator, argument_list.clone());
@@ -76,7 +98,7 @@ impl NativeOpLookup {
                     let i0: u32 = pair.get_item(0).extract()?;
                     let i1: PyRef<PyNode> = pair.get_item(1).extract()?;
                     let n = i1.clone();
-                    let r: Reduction<ArcSExp> = Reduction(i0, n.into());
+                    let r: Reduction<<ArcAllocator as Allocator>::Ptr> = Reduction(i0, n.into());
                     Ok(r)
                 }
             }
