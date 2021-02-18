216a4429e6e0fc0519d5e159962ebba9f455545e
Richard Kiss <him@richardkiss.com>
2021-02-18 11:41:09 -0800

    Fix benchmarks.

--- benchmark/run-benchmark.py
+++ benchmark/run-benchmark.py
@@ -6,7 +6,9 @@ import sys
 import os
 import time
 import random
-from clvm_rs import serialize_and_run_program, STRICT_MODE
+from clvm import KEYWORD_FROM_ATOM, KEYWORD_TO_ATOM
+from clvm.operators import OP_REWRITE
+from clvm_rs import deserialize_and_run_program, STRICT_MODE
 from colorama import init, Fore, Style
 
 init()
@@ -186,8 +188,22 @@ for n in range(5):
             program_data = bytes.fromhex(open(fn, 'r').read())
             env_data = bytes.fromhex(open(env_fn, 'r').read())
             time_start = time.perf_counter()
-            cost, result = serialize_and_run_program(
-                program_data, env_data, 1, 3, max_cost, flags)
+
+            native_opcode_names_by_opcode = dict(
+                ("op_%s" % OP_REWRITE.get(k, k), op)
+                for op, k in KEYWORD_FROM_ATOM.items()
+                if k not in "qa."
+            )
+            cost, result = deserialize_and_run_program(
+                program_data,
+                env_data,
+                KEYWORD_TO_ATOM["q"][0],
+                KEYWORD_TO_ATOM["a"][0],
+                native_opcode_names_by_opcode,
+                max_cost,
+                flags,
+            )
+
             time_end = time.perf_counter()
             output = ["run_program: %f" % (time_end - time_start)]
 
