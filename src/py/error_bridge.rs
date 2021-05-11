74f7d5b5f129702ce91b9762273b4990ebfa55a5
Richard Kiss <him@richardkiss.com>
2021-05-10 17:03:27 -0700

    Add `error_bridge` and bridge to allowing calling native `OpFn` from py.

---
+++ src/py/error_bridge.rs
@@ -0,0 +1,47 @@
+use crate::int_allocator::IntAllocator;
+use crate::reduction::EvalErr;
+use pyo3::types::{PyDict, PyString, PyTuple};
+use pyo3::{PyAny, PyErr, PyObject, PyResult, Python};
+
+use super::py_arena::PyArena;
+
+/// turn a `PyErr` into an `EvalErr<P>` if at all possible
+/// otherwise, return a `PyErr`
+pub fn eval_err_for_pyerr<'p>(
+    py: Python<'p>,
+    pyerr: &PyErr,
+    arena: &'p PyArena,
+    allocator: &mut IntAllocator,
+) -> PyResult<EvalErr<i32>> {
+    let args: &PyTuple = pyerr.pvalue(py).getattr("args")?.extract()?;
+    let arg0: &PyString = args.get_item(0).extract()?;
+    let sexp: &PyAny = pyerr.pvalue(py).getattr("_sexp")?.extract()?;
+    let node: i32 = arena.native_for_py(py, sexp, allocator)?;
+    let s: String = arg0.to_str()?.to_string();
+    Ok(EvalErr(node, s))
+}
+
+pub fn unwrap_or_eval_err<T, P>(obj: PyResult<T>, err_node: &P, msg: &str) -> Result<T, EvalErr<P>>
+where
+    P: Clone,
+{
+    match obj {
+        Err(_py_err) => Err(EvalErr(err_node.clone(), msg.to_string())),
+        Ok(o) => Ok(o),
+    }
+}
+
+pub fn raise_eval_error(py: Python, msg: &PyString, sexp: PyObject) -> PyResult<PyObject> {
+    let ctx: &PyDict = PyDict::new(py);
+    ctx.set_item("msg", msg)?;
+    ctx.set_item("sexp", sexp)?;
+    let r = py.run(
+        "from clvm.EvalError import EvalError; raise EvalError(msg, sexp)",
+        None,
+        Some(ctx),
+    );
+    match r {
+        Err(x) => Err(x),
+        Ok(_) => Ok(ctx.into()),
+    }
+}
--- src/py/native_op.rs
+++ src/py/native_op.rs
@@ -1,8 +1,16 @@
-use pyo3::prelude::pyclass;
+use pyo3::prelude::{pyclass, pymethods};
+use pyo3::types::PyString;
+use pyo3::{PyAny, PyCell, PyResult, Python, ToPyObject};
 
+use crate::allocator::Allocator;
+use crate::cost::Cost;
 use crate::int_allocator::IntAllocator;
+use crate::reduction::Reduction;
 
+use super::arena_object::ArenaObject;
+use super::error_bridge::{eval_err_for_pyerr, raise_eval_error};
 use super::f_table::OpFn;
+use super::py_arena::PyArena;
 
 //type OpFn<T> = fn(&mut T, <T as Allocator>::Ptr, Cost) -> Response<<T as Allocator>::Ptr>;
 
@@ -14,5 +22,35 @@ pub struct NativeOp {
 impl NativeOp {
     pub fn new(op: OpFn<IntAllocator>) -> Self {
         Self { op }
+    }
+}
+
+#[pymethods]
+impl NativeOp {
+    #[call]
+    fn __call__(&self, py: Python, args: &PyAny, _max_cost: Cost) -> PyResult<(Cost, ArenaObject)> {
+        let arena_cell = PyArena::new_cell(py)?;
+        let ptr = PyArena::include(arena_cell, py, args)?.borrow().get_ptr();
+        let arena: &PyArena = &arena_cell.borrow();
+        let mut allocator = arena.allocator();
+        let allocator: &mut IntAllocator = &mut allocator;
+        let r = (self.op)(allocator, ptr, _max_cost);
+        match r {
+            Ok(Reduction(cost, ptr)) => {
+                let r = ArenaObject::new(py, arena_cell, ptr);
+                Ok((cost, r))
+            }
+            Err(err) => {
+                let r = ArenaObject::new(py, arena_cell, ptr);
+                match raise_eval_error(
+                    py,
+                    PyString::new(py, "problem in suboperator"),
+                    PyCell::new(py, r)?.to_object(py),
+                ) {
+                    Err(e) => Err(e),
+                    Ok(_) => panic!("oh dear"),
+                }
+            }
+        }
     }
 }
