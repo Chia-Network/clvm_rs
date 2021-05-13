5f1c7134a3fc3ed8f49602cd87a452a22d09ded4
Richard Kiss <him@richardkiss.com>
2021-05-12 18:47:08 -0700

    Delete a bunch of stuff.

--- src/py/api.rs
+++ src/py/api.rs
@@ -1,24 +1,20 @@
 use std::cell::RefMut;
-use std::collections::HashMap;
 
 use pyo3::prelude::*;
-use pyo3::types::{PyBytes, PyDict, PyString};
+use pyo3::types::{PyBytes, PyDict};
 use pyo3::wrap_pyfunction;
 use pyo3::PyObject;
 
 use crate::core_ops::*;
-use crate::cost::Cost;
 use crate::err_utils::err;
 use crate::int_allocator::IntAllocator;
 use crate::more_ops::*;
 use crate::node::Node;
-use crate::reduction::{EvalErr, Reduction};
 use crate::serialize::node_to_bytes;
 
 use super::dialect::{Dialect, PyMultiOpFn};
 use super::f_table::OpFn;
 use super::native_op::NativeOp;
-use super::op_fn::PyOperatorHandler;
 use super::py_arena::PyArena;
 use super::run_program::{
     __pyo3_get_function_deserialize_and_run_program, __pyo3_get_function_serialized_length,
@@ -66,22 +62,6 @@ pub fn native_opcodes_dict(py: Python) -> PyResult<PyObject> {
 }
 
 #[pyfunction]
-fn raise_eval_error(py: Python, msg: &PyString, sexp: PyObject) -> PyResult<PyObject> {
-    let ctx: &PyDict = PyDict::new(py);
-    ctx.set_item("msg", msg)?;
-    ctx.set_item("sexp", sexp)?;
-    let r = py.run(
-        "from clvm.EvalError import EvalError; raise EvalError(msg, sexp)",
-        None,
-        Some(ctx),
-    );
-    match r {
-        Err(x) => Err(x),
-        Ok(_) => Ok(ctx.into()),
-    }
-}
-
-#[pyfunction]
 fn serialize_to_bytes<'p>(py: Python<'p>, sexp: &PyAny) -> PyResult<&'p PyBytes> {
     let arena = PyArena::new_cell(py)?;
     let arena_borrowed = arena.borrow();
@@ -112,54 +92,8 @@ fn clvm_rs(_py: Python, m: &PyModule) -> PyResult<()> {
 
     m.add("NATIVE_OP_UNKNOWN_NON_STRICT", PyMultiOpFn::new(op_unknown))?;
 
-    m.add_function(wrap_pyfunction!(py_run_program, m)?)?;
     m.add_function(wrap_pyfunction!(serialize_to_bytes, m)?)?;
     m.add_function(wrap_pyfunction!(deserialize_and_run_program, m)?)?;
 
     Ok(())
-}
-
-#[pyfunction]
-#[allow(clippy::too_many_arguments)]
-pub fn py_run_program<'p>(
-    py: Python<'p>,
-    program: &PyAny,
-    args: &PyAny,
-    quote_kw: &[u8],
-    apply_kw: &[u8],
-    max_cost: Cost,
-    opcode_lookup_by_name: HashMap<String, Vec<u8>>,
-    py_callback: PyObject,
-) -> PyResult<(Cost, PyObject)> {
-    let arena = PyArena::new_cell(py)?;
-    let arena_borrowed = arena.borrow();
-    let mut allocator_refcell: RefMut<IntAllocator> = arena_borrowed.allocator();
-    let allocator: &mut IntAllocator = &mut allocator_refcell as &mut IntAllocator;
-
-    let op_lookup = PyOperatorHandler::new(opcode_lookup_by_name, py_callback, arena)?;
-    let program = PyArena::native_for_py(arena, py, program, allocator)?;
-    let args = PyArena::native_for_py(arena, py, args, allocator)?;
-
-    let r: Result<Reduction<i32>, EvalErr<i32>> = crate::run_program::run_program(
-        allocator, &program, &args, quote_kw, apply_kw, max_cost, &op_lookup, None,
-    );
-
-    match r {
-        Ok(reduction) => {
-            let r = arena_borrowed.py_for_native(py, &reduction.1, allocator)?;
-            Ok((reduction.0, r.to_object(py)))
-        }
-        Err(eval_err) => {
-            let node: PyObject = arena_borrowed
-                .py_for_native(py, &eval_err.0, allocator)?
-                .to_object(py);
-            let s: String = eval_err.1;
-            let s1: &str = &s;
-            let msg: &PyString = PyString::new(py, s1);
-            match raise_eval_error(py, &msg, node) {
-                Err(x) => Err(x),
-                _ => panic!(),
-            }
-        }
-    }
 }
