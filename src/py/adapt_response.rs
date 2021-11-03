ed3efa9f588a8ca97971bb1a5205d5086f62005c
Richard Kiss <him@richardkiss.com>
2021-11-02 18:49:45 -0700

    Factor out `eval_err_to_pyresult`.

--- src/py/adapt_response.rs
+++ src/py/adapt_response.rs
@@ -1,11 +1,28 @@
 use crate::allocator::Allocator;
 use crate::py::lazy_node::LazyNode;
-use crate::reduction::Response;
+use crate::reduction::{EvalErr, Response};
 
 use std::rc::Rc;
 
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
+
+pub fn eval_err_to_pyresult<T>(py: Python, eval_err: EvalErr, allocator: Allocator) -> PyResult<T> {
+    let node = LazyNode::new(Rc::new(allocator), eval_err.0);
+    let msg = eval_err.1;
+    let ctx: &PyDict = PyDict::new(py);
+    ctx.set_item("msg", msg)?;
+    ctx.set_item("node", node)?;
+    Err(py
+        .run(
+            "
+from clvm.EvalError import EvalError
+raise EvalError(msg, node)",
+            None,
+            Some(ctx),
+        )
+        .unwrap_err())
+}
 
 pub fn adapt_response_to_py(
     py: Python,
@@ -17,21 +34,6 @@ pub fn adapt_response_to_py(
             let val = LazyNode::new(Rc::new(allocator), reduction.1);
             Ok((reduction.0, val))
         }
-        Err(eval_err) => {
-            let node = LazyNode::new(Rc::new(allocator), eval_err.0);
-            let msg = eval_err.1;
-            let ctx: &PyDict = PyDict::new(py);
-            ctx.set_item("msg", msg)?;
-            ctx.set_item("node", node)?;
-            Err(py
-                .run(
-                    "
-from clvm.EvalError import EvalError
-raise EvalError(msg, node)",
-                    None,
-                    Some(ctx),
-                )
-                .unwrap_err())
-        }
+        Err(eval_err) => eval_err_to_pyresult(py, eval_err, allocator),
     }
 }
