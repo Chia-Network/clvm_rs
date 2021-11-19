a2fc24e6bfb60de5e1304136b91d76938cac1136
arvidn <arvid@libtorrent.org>
2021-11-19 17:43:54 +0100

    fixup

--- build.rs
+++ build.rs
@@ -4,10 +4,14 @@ fn main() {
         println!("cargo:rustc-link-lib=mpir");
         println!("cargo:rustc-link-search=mpir_gc_x64");
     }
-    #[cfg(not(windows))]
+    #[cfg(linux)]
+    {
+        println!("cargo:rustc-link-lib=libgmp.so.3");
+        println!("cargo:rustc-link-search=/usr/lib64");
+    }
+    #[cfg(macos)]
     {
         println!("cargo:rustc-link-lib=gmp");
         println!("cargo:rustc-link-search=/opt/homebrew/lib");
-        println!("cargo:rustc-link-search=/usr/lib64");
     }
 }
