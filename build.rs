b22bfb1c6a1eb821fee5d62073632324e4d1a874
arvidn <arvid@libtorrent.org>
2021-11-23 14:44:17 +0100

    use features to control what we link against

--- build.rs
+++ build.rs
@@ -1,18 +1,24 @@
+use std::env;
+
 fn main() {
-    #[cfg(target_os = "windows")]
-    {
+    if env::var_os("CARGO_FEATURE_MPIR").is_some() {
         println!("cargo:rustc-link-lib=mpir");
         println!("cargo:rustc-link-search=mpir_gc_x64");
+    } else if env::var_os("CARGO_FEATURE_LIBGMP3").is_some() {
+        println!("cargo:rustc-link-lib=libgmp.so.3");
+    } else if env::var_os("CARGO_FEATURE_LIBGMP10").is_some() {
+        println!("cargo:rustc-link-lib=libgmp.so.10");
+    } else if env::var_os("CARGO_FEATURE_LIBGMP").is_some() {
+        println!("cargo:rustc-link-lib=gmp");
     }
+
     #[cfg(target_os = "linux")]
     {
-        println!("cargo:rustc-link-lib=gmp");
         println!("cargo:rustc-link-search=/usr/lib64");
         println!("cargo:rustc-link-search=/usr/lib");
     }
     #[cfg(target_os = "macos")]
     {
-        println!("cargo:rustc-link-lib=gmp");
         println!("cargo:rustc-link-search=/opt/homebrew/lib");
     }
 }
