4764d6be3a6af991ac69d6b7ae11a6d8561fd8a4
Richard Kiss <him@richardkiss.com>
2022-07-01 21:00:40 -0700

    simplify

--- src/cmds/swbr.rs
+++ src/cmds/swbr.rs
@@ -1,30 +1,11 @@
 use clap::Parser;
-use clvmr::allocator::{Allocator, NodePtr};
-use clvmr::compressor::{compress_with_backrefs, decompress};
-use clvmr::node::Node;
-use clvmr::serialize::{node_to_bytes, node_to_bytes_backrefs};
+use clvmr::compressor::reserialize;
 use hex::{decode, encode};
-use std::error::Error;
-
-struct AllocatorNode {
-    allocator: Allocator,
-    node_ptr: NodePtr,
-}
-
-fn node_from_hex(s: &str) -> Result<AllocatorNode, Box<dyn Error + Send + Sync + 'static>> {
-    let input_program = decode(s)?;
-    let mut allocator = Allocator::new();
-    let node_ptr = decompress(&mut allocator, &input_program)?;
-    Ok(AllocatorNode {
-        allocator,
-        node_ptr,
-    })
-}
 
 #[derive(Parser)]
 struct Cli {
-    #[clap(parse(try_from_str = node_from_hex))]
-    input_program: AllocatorNode,
+    #[clap(parse(try_from_str = decode))]
+    input_program: Vec<Vec<u8>>,
 
     #[clap(short, long)]
     uncompressed_output: bool,
@@ -36,17 +17,8 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    let mut allocator = args.input_program.allocator;
-    let node_ptr = args.input_program.node_ptr;
-
-    let blob = if args.include_deserialize_program {
-        compress_with_backrefs(&mut allocator, node_ptr)
-    } else if args.uncompressed_output {
-        node_to_bytes(&Node::new(&allocator, node_ptr))
-    } else {
-        node_to_bytes_backrefs(&Node::new(&allocator, node_ptr))
-    }
-    .expect("bad serialization");
+    let blob = reserialize(&args.input_program[0], args.include_deserialize_program)
+        .expect("bad serialization");
 
     let output = encode(blob);
     println!("{}", output);
