ecdc1c1d710516bcaadfef76c9f2b2a9472af697
Richard Kiss <him@richardkiss.com>
2022-07-15 15:43:02 -0700

    rebase fixup

--- src/reduction.rs
+++ src/reduction.rs
@@ -1,4 +1,3 @@
-use std::error::Error;
 use std::io;
 
 use crate::allocator::NodePtr;
@@ -15,19 +14,5 @@ pub type Response = Result<Reduction, EvalErr>;
 impl From<EvalErr> for io::Error {
     fn from(v: EvalErr) -> Self {
         Self::new(io::ErrorKind::Other, v.1)
-    }
-}
-
-impl std::fmt::Display for EvalErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "{}", self.1)
-    }
-}
-
-impl Error for EvalErr {}
-
-impl From<Box<dyn Error>> for EvalErr {
-    fn from(err: Box<dyn Error>) -> Self {
-        EvalErr(0, err.to_string())
     }
 }
