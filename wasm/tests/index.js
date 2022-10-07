6a3b06cafd8d568576465935d2cc77f0b267b421
ChiaMineJP <admin@chiamine.jp>
2022-10-08 01:48:31 +0900

    Updated test code for wasm

--- wasm/tests/index.js
+++ wasm/tests/index.js
@@ -1,8 +1,8 @@
 const wasm = require("../pkg/clvm_wasm.js");
 
-function assert(value){
-    if(!value){
-        throw new Error("assertion error");
+function assert(challenge, expected){
+    if(challenge !== expected){
+        throw new Error(`Assertion Error: Expected "${expected}" but actual value was "${challenge}"`);
     }
 }
 
@@ -12,7 +12,7 @@ let prog1 = Uint8Array.from(Buffer.from("ff017f", "hex"));
 let arg1 = Uint8Array.from(Buffer.from("00", "hex"));
 let [cost1, sexp1] = wasm.run_chia_program(prog1, arg1, BigInt("100000000000"), 0);
 
-assert(sexp1.atom.toString() === "127");
+assert(sexp1.atom.toString(), "127");
 
 // (+ 1 (q . 3))
 let prog2 = Uint8Array.from(Buffer.from("ff10ff01ffff010380", "hex"));
@@ -20,5 +20,4 @@ let prog2 = Uint8Array.from(Buffer.from("ff10ff01ffff010380", "hex"));
 let arg2 = Uint8Array.from(Buffer.from("02", "hex"));;
 let [cost2, sexp2] = wasm.run_chia_program(prog2, arg2, BigInt("100000000000"), 0);
 
-// assert(sexp2.atom.toString() === "5");
-assert(sexp2.atom.toString() === "xxxx"); // Make it fail to check whether CI action actually fails.
+assert(sexp2.atom.toString(), "5");
