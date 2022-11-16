b43616da503d0f86226c63951d8f1c50cfc0d13d
Richard Kiss <him@richardkiss.com>
2022-11-16 15:59:49 -0800

    Further refactor serde.

--- src/serde/mod.rs
+++ src/serde/mod.rs
@@ -1,6 +1,10 @@
 mod de;
+mod errors;
+mod parse_atom;
 mod ser;
+mod tools;
 mod write_atom;
 
-pub use de::{node_from_bytes, serialized_length_from_bytes, tree_hash_from_stream};
+pub use de::node_from_bytes;
 pub use ser::node_to_bytes;
+pub use tools::{serialized_length_from_bytes, tree_hash_from_stream};
