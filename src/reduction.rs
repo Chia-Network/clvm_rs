1305959e47aba887d150aca92eae643c5453253d
Rigidity <me@rigidnetwork.com>
2023-08-09 17:13:11 -0400

    Implement Display and Error for EvalErr

--- src/reduction.rs
+++ src/reduction.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{fmt, io};
 
 use crate::allocator::NodePtr;
 use crate::cost::Cost;
@@ -10,6 +10,14 @@ pub struct EvalErr(pub NodePtr, pub String);
 pub struct Reduction(pub Cost, pub NodePtr);
 
 pub type Response = Result<Reduction, EvalErr>;
+
+impl fmt::Display for EvalErr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Error at {:?}: {}", self.0, self.1)
+    }
+}
+
+impl std::error::Error for EvalErr {}
 
 impl From<EvalErr> for io::Error {
     fn from(v: EvalErr) -> Self {
