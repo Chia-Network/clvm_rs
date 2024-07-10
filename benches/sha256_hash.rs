7a252d43c2860510cf591177c6d9f0bb165f9fe9
Jack Nelson <jack@jacknelson.xyz>
2024-07-10 17:47:29 -0400

    make pretty

--- benches/sha256_hash.rs
+++ benches/sha256_hash.rs
@@ -1,18 +1,19 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, criterion_group, criterion_main};
 
 use clvmr::sha2::Sha256;
 
 const BYTE_LENGTHS: [u8; 6] = [8, 16, 32, 64, 96, 128];
 const MAX_VAL: u8 = 250;
-fn gen_bytes(value: &u8, amount: &u8) -> Vec<u8> {
+
+fn gen_bytes(value: u8, amount: u8) -> Vec<u8> {
     let mut bytes = Vec::new();
-    for _ in 0..*amount {
-        bytes.push(*value);
+    for _ in 0..amount {
+        bytes.push(value);
     }
     bytes
 }
 
-fn hash_bytes(bytes: Vec<u8>) -> [u8; 32] {
+fn hash_bytes(bytes: &[u8]) -> [u8; 32] {
     let mut sha256 = Sha256::new();
     sha256.update(&bytes);
     sha256.finalize()
@@ -27,8 +28,8 @@ fn sha256_hash_benchmark(c: &mut Criterion) {
             // this figures out how many iterations to run.
             for val in 0..MAX_VAL {
                 for len in BYTE_LENGTHS {
-                    let bytes = gen_bytes(&val, &len);
-                    hash_bytes(bytes); // purposely not borrowing bytes, as no longer needed.
+                    let bytes = gen_bytes(val, len);
+                    hash_bytes(&bytes);
                 }
             }
         })
