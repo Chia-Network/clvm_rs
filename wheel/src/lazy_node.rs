8a4afd264b48c92e1c27a3054768908ae6392187
Rigidity <me@rigidnetwork.com>
2024-07-11 09:33:19 -0400

    Other dependency updates

--- wheel/src/lazy_node.rs
+++ wheel/src/lazy_node.rs
@@ -13,9 +13,8 @@ pub struct LazyNode {
 
 impl ToPyObject for LazyNode {
     fn to_object(&self, py: Python<'_>) -> PyObject {
-        let node: &PyCell<LazyNode> = PyCell::new(py, self.clone()).unwrap();
-        let pa: &PyAny = node;
-        pa.to_object(py)
+        let node: Bound<LazyNode> = Bound::new(py, self.clone()).unwrap();
+        node.to_object(py)
     }
 }
 
@@ -27,7 +26,7 @@ impl LazyNode {
             SExp::Pair(p1, p2) => {
                 let r1 = Self::new(self.allocator.clone(), *p1);
                 let r2 = Self::new(self.allocator.clone(), *p2);
-                let v: &PyTuple = PyTuple::new(py, &[r1, r2]);
+                let v = PyTuple::new_bound(py, &[r1, r2]);
                 Ok(Some(v.into()))
             }
             _ => Ok(None),
@@ -37,7 +36,9 @@ impl LazyNode {
     #[getter(atom)]
     pub fn atom(&self, py: Python) -> Option<PyObject> {
         match &self.allocator.sexp(self.node) {
-            SExp::Atom => Some(PyBytes::new(py, self.allocator.atom(self.node).as_ref()).into()),
+            SExp::Atom => {
+                Some(PyBytes::new_bound(py, self.allocator.atom(self.node).as_ref()).into())
+            }
             _ => None,
         }
     }
