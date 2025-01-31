a82e47f2d8d52cf3be579b8d3ccfb7f29ff8e3d8
Matthew Howard <m.o.howard97@gmail.com>
2025-01-31 16:11:21 +0000

    disable checkpoint

--- fuzz/fuzz_targets/deserialize_br.rs
+++ fuzz/fuzz_targets/deserialize_br.rs
@@ -16,10 +16,10 @@ fuzz_target!(|data: &[u8]| {
 
     let b1 = node_to_bytes_backrefs(&allocator, program).unwrap();
 
-    let cp = allocator.checkpoint();
+    // let cp = allocator.checkpoint();
     let program = node_from_bytes_backrefs(&mut allocator, &b1).unwrap();
     // let new_pair_count = allocator.pair_count();
-    allocator.restore_checkpoint(&cp);
+    // allocator.restore_checkpoint(&cp);
     let program_old = node_from_bytes_backrefs_old(&mut allocator, &b1).unwrap();
     // assert!(new_pair_count <= allocator.pair_count());
     let b3 = node_to_bytes_backrefs(&allocator, program_old).unwrap();
