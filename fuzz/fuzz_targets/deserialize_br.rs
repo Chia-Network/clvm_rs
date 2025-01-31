24abb5a0988e801d97cc6eb97bfa4f88cae8064e
Matthew Howard <m.o.howard97@gmail.com>
2025-01-31 15:59:43 +0000

    check old first

--- fuzz/fuzz_targets/deserialize_br.rs
+++ fuzz/fuzz_targets/deserialize_br.rs
@@ -22,10 +22,11 @@ fuzz_target!(|data: &[u8]| {
     allocator.restore_checkpoint(&cp);
     let program_old = node_from_bytes_backrefs_old(&mut allocator, &b1).unwrap();
     // assert!(new_pair_count <= allocator.pair_count());
-    let b2 = node_to_bytes_backrefs(&allocator, program).unwrap();
-    assert_eq!(b1, b2);
     let b3 = node_to_bytes_backrefs(&allocator, program_old).unwrap();
     assert_eq!(b1, b3);
+    let b2 = node_to_bytes_backrefs(&allocator, program).unwrap();
+    assert_eq!(b1, b2);
+    
 });
 
 // // #[cfg(feature = "counters")]
