c6a4071065d4b32d7fb4d558b858d9ad38b4f297
Matthew Howard <m.o.howard97@gmail.com>
2025-01-31 13:42:27 +0000

    check original version of fuzzer

--- fuzz/fuzz_targets/deserialize_br.rs
+++ fuzz/fuzz_targets/deserialize_br.rs
@@ -22,11 +22,32 @@ fuzz_target!(|data: &[u8]| {
     let new_pair_count = allocator.pair_count();
     allocator.restore_checkpoint(&cp);
     let program_old = node_from_bytes_backrefs_old(&mut allocator, &b1).unwrap();
-    // check we aren't making more nodes in the new version
     assert!(new_pair_count <= allocator.pair_count());
-    // check that the two versions of the deserializer produce the same/correct result
     let b2 = node_to_bytes_backrefs(&allocator, program).unwrap();
     assert_eq!(b1, b2);
     let b3 = node_to_bytes_backrefs(&allocator, program_old).unwrap();
     assert_eq!(b1, b3);
 });
+
+// // #[cfg(feature = "counters")]
+// fuzz_target!(|data: &[u8]| {
+//     let mut allocator = Allocator::new();
+//     let cp = allocator.checkpoint();
+//     let program = match node_from_bytes_backrefs(&mut allocator, data) {
+//         Err(_) => {
+//             return;
+//         }
+//         Ok(r) => r,
+//     };
+
+//     let b1 = node_to_bytes_backrefs(&allocator, program).unwrap();
+
+//     // reset allocators
+//     allocator.restore_checkpoint(&cp);
+
+//     let _program = node_from_bytes_backrefs(&mut allocator, &b1).unwrap();
+//     let new_pair_count = allocator.pair_count();
+//     allocator.restore_checkpoint(&cp);
+//     let _program_old = node_from_bytes_backrefs_old(&mut allocator, &b1).unwrap();
+//     assert!(new_pair_count <= allocator.pair_count());
+// });
