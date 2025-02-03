afe636f31b7fb5d904b6f2d576d4cbe9cb602a9d
Matthew Howard <m.o.howard97@gmail.com>
2025-02-03 15:38:46 +0000

    remove commented out code

--- fuzz/fuzz_targets/deserialize_br.rs
+++ fuzz/fuzz_targets/deserialize_br.rs
@@ -25,26 +25,3 @@ fuzz_target!(|data: &[u8]| {
 
     assert!(node_eq::node_eq(&allocator, program, program_old));
 });
-
-// // #[cfg(feature = "counters")]
-// fuzz_target!(|data: &[u8]| {
-//     let mut allocator = Allocator::new();
-//     let cp = allocator.checkpoint();
-//     let program = match node_from_bytes_backrefs(&mut allocator, data) {
-//         Err(_) => {
-//             return;
-//         }
-//         Ok(r) => r,
-//     };
-
-//     let b1 = node_to_bytes_backrefs(&allocator, program).unwrap();
-
-//     // reset allocators
-//     allocator.restore_checkpoint(&cp);
-
-//     let _program = node_from_bytes_backrefs(&mut allocator, &b1).unwrap();
-//     let new_pair_count = allocator.pair_count();
-//     allocator.restore_checkpoint(&cp);
-//     let _program_old = node_from_bytes_backrefs_old(&mut allocator, &b1).unwrap();
-//     assert!(new_pair_count <= allocator.pair_count());
-// });
