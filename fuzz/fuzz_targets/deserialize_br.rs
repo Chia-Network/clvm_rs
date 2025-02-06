cdd627ef3a987ef5243101564802102f280a42be
Matthew Howard <m.o.howard97@gmail.com>
2025-02-06 12:59:23 +0000

    further simplify fuzz for de_brs

--- fuzz/fuzz_targets/deserialize_br.rs
+++ fuzz/fuzz_targets/deserialize_br.rs
@@ -5,7 +5,6 @@ mod node_eq;
 use clvmr::allocator::Allocator;
 use clvmr::serde::node_from_bytes_backrefs;
 use clvmr::serde::node_from_bytes_backrefs_old;
-use clvmr::serde::node_to_bytes_backrefs;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
@@ -18,11 +17,6 @@ fuzz_target!(|data: &[u8]| {
         Ok(r) => r,
     };
 
-    let program_old = node_from_bytes_backrefs_old(&mut allocator, data);
-
-    assert!(!(program.is_err() ^ program_old.is_err()));
-
-    let program = program.unwrap();
-    let program_old = program_old.unwrap();
+    let program_old = node_from_bytes_backrefs_old(&mut allocator, data).unwrap();
     assert!(node_eq::node_eq(&allocator, program, program_old));
 });
