494f536244d1d57169661dd74512c0b035f130e0
Matthew Howard <m.o.howard97@gmail.com>
2025-02-10 14:46:45 +0000

    fmt fuzz

--- fuzz/fuzz_targets/deserialize_br.rs
+++ fuzz/fuzz_targets/deserialize_br.rs
@@ -13,7 +13,7 @@ fuzz_target!(|data: &[u8]| {
     let node_count = allocator.node_count();
     let res2 = node_from_bytes_backrefs_old(&mut allocator, data);
     // check that the new implementation creates the same number of nodes as the old one
-    assert_eq!(node_count * 2, allocator.node_count());  
+    assert_eq!(node_count * 2, allocator.node_count());
     match (res1, res2) {
         (Err(e1), Err(e2)) => {
             assert_eq!(e1, e2);
