517ec32b4c30db7b9562e7eef1652dd2d8c5b1a2
arvidn <arvid@libtorrent.org>
2025-02-23 14:56:59 +0100

    Add two new serializer benchmarks trees

--- benches/serialize.rs
+++ benches/serialize.rs
@@ -1,6 +1,7 @@
 use clvmr::allocator::Allocator;
 use clvmr::serde::{
-    node_from_bytes, node_from_bytes_backrefs, node_to_bytes, node_to_bytes_backrefs, Serializer,
+    node_from_bytes, node_from_bytes_backrefs, node_to_bytes_backrefs, node_to_bytes_limit,
+    Serializer,
 };
 use criterion::black_box;
 use criterion::{criterion_group, criterion_main, Criterion};
@@ -14,15 +15,28 @@ fn serialize_benchmark(c: &mut Criterion) {
     let block0: &[u8] = include_bytes!("0.generator");
     let block1: &[u8] = include_bytes!("1.generator");
     let block2: &[u8] = include_bytes!("2.generator");
+    let block3: &[u8] = include_bytes!("3.generator");
+    let block4: &[u8] = include_bytes!("4.generator");
 
     let mut group = c.benchmark_group("serialize");
 
-    for (block, name) in [(&block0, "0"), (&block1, "1"), (&block2, "2")] {
+    for (block, name) in [
+        (&block0, "0"),
+        (&block1, "1"),
+        (&block2, "2"),
+        (&block3, "3"),
+        (&block4, "4"),
+    ] {
         let mut a = Allocator::new();
         let node = node_from_bytes_backrefs(&mut a, block).expect("node_from_bytes_backrefs");
-        let inflated = node_to_bytes(&a, node).expect("node_to_bytes");
-        let mut a = Allocator::new();
-        let node = node_from_bytes(&mut a, inflated.as_slice()).expect("node_from_bytes");
+
+        // if the inflated form takes too much space, just run the benchmark on the compact form
+        let node = if let Ok(inflated) = node_to_bytes_limit(&a, node, 2000000) {
+            a = Allocator::new();
+            node_from_bytes(&mut a, inflated.as_slice()).expect("node_from_bytes")
+        } else {
+            node
+        };
 
         group.bench_function(format!("node_to_bytes_backrefs {name}"), |b| {
             b.iter(|| {
