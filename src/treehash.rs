056abac5213708fe5ea8bbe7f16109f70b5cb117
Matthew Howard <m.o.howard97@gmail.com>
2025-10-28 11:50:50 +0000

    make treeop private

--- src/treehash.rs
+++ src/treehash.rs
@@ -1,8 +1,17 @@
+use crate::allocator::NodeVisitor;
 use crate::allocator::{Allocator, NodePtr};
+use crate::cost::check_cost;
 use crate::cost::Cost;
+use crate::more_ops::PRECOMPUTED_HASHES;
+use crate::reduction::Reduction;
+use crate::reduction::Response;
 use crate::ObjectType;
 use crate::SExp;
 use chia_sha2::Sha256;
+
+const SHA256TREE_BASE_COST: Cost = 0;
+const SHA256TREE_COST_PER_CALL: Cost = 1300;
+const SHA256TREE_COST_PER_BYTE: Cost = 10;
 
 pub fn tree_hash_atom(bytes: &[u8]) -> [u8; 32] {
     let mut sha256 = Sha256::new();
@@ -122,8 +131,85 @@ impl TreeCache {
     }
 }
 
-pub(crate) enum TreeOp {
+enum TreeOp {
     SExp(NodePtr),
     Cons,
     ConsAddCacheCost(NodePtr, Cost),
+}
+
+pub fn tree_hash_cached_costed(
+    a: &mut Allocator,
+    node: NodePtr,
+    cache: &mut TreeCache,
+    cost_left: Cost,
+) -> Response {
+    cache.visit_tree(a, node);
+
+    let mut hashes = Vec::new();
+    let mut ops = vec![TreeOp::SExp(node)];
+    let mut cost: Cost = SHA256TREE_BASE_COST;
+
+    while let Some(op) = ops.pop() {
+        // charge a call cost for processing this op
+        cost += SHA256TREE_COST_PER_CALL;
+        check_cost(cost, cost_left)?;
+        match op {
+            TreeOp::SExp(node) => match a.node(node) {
+                NodeVisitor::Buffer(bytes) => {
+                    cost += SHA256TREE_COST_PER_BYTE * bytes.len() as u64;
+                    check_cost(cost, cost_left)?;
+                    let hash = tree_hash_atom(bytes);
+                    hashes.push(hash);
+                }
+                NodeVisitor::U32(val) => {
+                    cost += SHA256TREE_COST_PER_BYTE * a.atom_len(node) as u64;
+                    check_cost(cost, cost_left)?;
+                    if (val as usize) < PRECOMPUTED_HASHES.len() {
+                        hashes.push(PRECOMPUTED_HASHES[val as usize]);
+                    } else {
+                        hashes.push(tree_hash_atom(a.atom(node).as_ref()));
+                    }
+                }
+                NodeVisitor::Pair(left, right) => {
+                    // pair cost (65 bytes as before)
+                    cost += SHA256TREE_COST_PER_BYTE * 65_u64;
+                    check_cost(cost, cost_left)?;
+                    if let Some((hash, cached_cost)) = cache.get(node) {
+                        // when reusing a cached subtree, charge its cached cost
+                        cost += cached_cost;
+                        check_cost(cost, cost_left)?;
+                        hashes.push(*hash);
+                    } else {
+                        if cache.should_memoize(node) {
+                            // record the cost_left before traversing this subtree
+                            ops.push(TreeOp::ConsAddCacheCost(node, cost));
+                        } else {
+                            ops.push(TreeOp::Cons);
+                        }
+                        ops.push(TreeOp::SExp(left));
+                        ops.push(TreeOp::SExp(right));
+                    }
+                }
+            },
+            TreeOp::Cons => {
+                let first = hashes.pop().unwrap();
+                let rest = hashes.pop().unwrap();
+                hashes.push(tree_hash_pair(first, rest));
+            }
+            TreeOp::ConsAddCacheCost(original_node, cost_before) => {
+                let first = hashes.pop().unwrap();
+                let rest = hashes.pop().unwrap();
+                let hash = tree_hash_pair(first, rest);
+                hashes.push(hash);
+                // cost_before will be lower
+                // cost_left is the remaining after computing it
+                // the cost of this subtree = after - before
+                let used = cost - cost_before;
+                cache.insert(original_node, &hash, used);
+            }
+        }
+    }
+
+    assert!(hashes.len() == 1);
+    Ok(Reduction(cost, a.new_atom(&hashes[0])?))
 }
