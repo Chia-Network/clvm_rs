9fa2495ed2a6ee80732795bd1553af3b0d0d770b
Matthew Howard <m.o.howard97@gmail.com>
2025-11-11 16:36:45 +0000

    new per-byte cost measuring

--- tools/src/bin/sha256tree-benching.rs
+++ tools/src/bin/sha256tree-benching.rs
@@ -1,3 +1,5 @@
+use clvmr::chia_dialect::ChiaDialect;
+use clvmr::run_program::run_program;
 use std::fs::File;
 use std::io::Write;
 use std::time::Instant;
@@ -6,15 +8,7 @@ use linreg::linear_regression_of;
 
 // bring in your existing code
 use clvmr::allocator::{Allocator, NodePtr};
-use clvmr::treehash::{tree_hash, tree_hash_atom};
-
-fn make_nested_pairs(a: &mut Allocator, depth: usize) -> NodePtr {
-    let mut node = a.nil();
-    for _ in 0..depth {
-        node = a.new_pair(node, a.nil()).unwrap();
-    }
-    node
-}
+use clvmr::treehash::tree_hash;
 
 fn make_list_of_atoms(a: &mut Allocator, n: usize) -> NodePtr {
     let atom = a.new_atom(&[1u8; 32]).unwrap();
@@ -25,6 +19,36 @@ fn make_list_of_atoms(a: &mut Allocator, n: usize) -> NodePtr {
     list
 }
 
+fn time_per_byte_for_atom(a: &mut Allocator, output: &mut dyn Write) -> (f64, f64) {
+    let mut samples = Vec::<(f64, f64)>::new();
+    let dialect = ChiaDialect::new(0x0200); // enable shatree
+
+    let op_code = a.new_number(65.into()).unwrap();
+    let quote = a.new_number(1.into()).unwrap();
+    let mut atom_str = String::from("");
+    let checkpoint = a.checkpoint();
+
+    for i in (0..1000000).step_by(5) {
+        // make the atom longer as a function of i
+        atom_str.push_str(&((i % 89) + 10).to_string()); // just to mix it up
+        let atom = a.new_atom(&hex::decode(&atom_str).unwrap()).unwrap();
+        // let args = a.new_pair(atom, a.nil()).unwrap();
+        let args = a.new_pair(quote, atom).unwrap();
+        let call = a.new_pair(args, a.nil()).unwrap();
+        let call = a.new_pair(op_code, call).unwrap();
+        let start = Instant::now();
+        run_program(a, &dialect, call, a.nil(), 11000000000).unwrap();
+        let duration = start.elapsed();
+        let sample = (i as f64, duration.as_nanos() as f64);
+        writeln!(output, "{}\t{}", sample.0, sample.1).expect("failed to write");
+        samples.push(sample);
+
+        a.restore_checkpoint(&checkpoint);
+    }
+
+    linear_regression_of(&samples).expect("linreg failed")
+}
+
 fn main() -> std::io::Result<()> {
     let mut output = File::create("sha256tree_costs.tsv")?;
 
@@ -33,48 +57,17 @@ fn main() -> std::io::Result<()> {
     // It's calibrated against the timing of point_add, which has a cost
     let cost_scale = ((101094.0 / 39000.0) + (1343980.0 / 131000.0)) / 2.0;
 
-    // cost call (with nil)
-    let mut samples = vec![];
+    // base call cost is covered in benchmark-clvm-cost so not included here
+    // cost atom sizes
     {
         let mut a = Allocator::new();
-        for depth in 1..1000 {
-            let node = make_nested_pairs(&mut a, depth);
-            let start = Instant::now();
-            tree_hash(&a, node); // using tree hash as it costs the same as cached
-            let t = start.elapsed().as_nanos() as f64;
-            writeln!(output, "call\t{}\t{}", depth, t)?;
-            samples.push((depth as f64, t));
-        }
-        let (slope, intercept): (f64, f64) = linear_regression_of(&samples).expect("linreg failed");
-        let cost = slope * cost_scale;
-        writeln!(output, "\n# call_slope\t{:.9}", slope)?;
-        writeln!(output, "\n# call_slope * cost_scale\t{:?}", cost)?;
-        writeln!(output, "# call_intercept\t{:.9}\n", intercept)?;
-        println!(
-            "call slope: {:.9}, intercept: {:.9}, cost: {:.9}",
-            slope, intercept, cost
-        );
-    }
-
-    // cost atom sizes
-    let mut samples = vec![];
-    {
-        for size in 1..8192 {
-            let atom: Vec<u8> = vec![11_u8; size];
-            let start = Instant::now();
-            tree_hash_atom(&atom);
-            let t = start.elapsed().as_nanos() as f64;
-
-            writeln!(output, "atom\t{}\t{}", size, t)?;
-            samples.push((size as f64, t));
-        }
-        let (slope, intercept): (f64, f64) = linear_regression_of(&samples).expect("linreg failed");
+        let (slope, intercept) = time_per_byte_for_atom(&mut a, &mut output);
         let cost = slope * cost_scale;
         writeln!(output, "\n# atom_slope\t{:.9}", slope)?;
         writeln!(output, "\n# atom_slope * cost_scale\t{:?}", cost)?;
         writeln!(output, "# atom_intercept\t{:.9}\n", intercept)?;
         println!(
-            "atom slope: {:.9}, intercept: {:.9}, cost: {:.9}",
+            "atom slope: {:.9}, intercept: {:.9}, cost (slope * cost_scale): {:.9}",
             slope, intercept, cost
         );
     }
